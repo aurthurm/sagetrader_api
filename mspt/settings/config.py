21ab134e47ac8edc00ccf6fa628df7682592b863
Aurthur Musendame <aurthurmusendame@gmail.com>
2021-12-13 13:14:21 +0200

    added static secret key

--- mspt/settings/config.py
+++ mspt/settings/config.py
@@ -17,7 +17,7 @@ API_V1_STR = "/api/v1"
 
 SECRET_KEY = os.getenvb(b"SECRET_KEY")
 if not SECRET_KEY:
-    SECRET_KEY = os.urandom(32)
+    SECRET_KEY = "DURdlxzKW4LsTaDWKioKuiL8oimGY0o0j5Z_ztT-q84"  # secrets.token_urlsafe(32)
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 8  # 60 minutes * 24 hours * 8 days = 8 days
 
